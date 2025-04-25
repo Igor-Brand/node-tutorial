fb66924ea7e659aadf42e4b4e4de8451e275dbaa
Punkrig <igorbrandaao@gmail.com>
2025-04-25 17:50:57 -0300

    Desenvolvimento: aula 05

--- 05tut/server.js
+++ 05tut/server.js
@@ -10,11 +10,37 @@ class Emitter extends EventEmitter{};
 
 // iniciando o objeto
 const myEmitter = new Emitter();
+myEmitter.on('log',(msg,fileName)=>logEvents(msg, fileName));
 
 const PORT = process.env.PORT || 3500;
 
+const serveFile = async (filePath, contentType, response) =>{
+
+    try{
+        const rawData = await fsPromises.readFile(
+            filePath, 
+            !contentType.includes('image') ? 'utf8' : ''
+        );
+        const data = contentType === 'application/json'
+            ? JSON.parse(rawData) : rawData;
+        response.writeHead(
+            filePath.includes('404.html') ? 404: 200, 
+            {'Content-Type':contentType});
+        response.end(
+            contentType === 'application/json' ? JSON.stringify(data): data
+        );
+    }catch(err){
+        console.log(err);
+        myEmitter.emit('log',`${err.name}: ${err.message}`,'errLog.txt');
+        response.statusCode= 500;
+        response.end();
+    }
+}
+
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method);
+
+    myEmitter.emit('log',`${req.url}\t${req.method}`,'reqLog.txt');
 
     const extension = path.extname(req.url);
 
@@ -40,7 +66,7 @@ const server = http.createServer((req,res)=>{
             contentType = 'text/plain';
             break;
         default:
-            contentType: 'text/html';
+            contentType= 'text/html';
     }
 
     let filePath =
@@ -51,12 +77,41 @@ const server = http.createServer((req,res)=>{
                 : contentType === 'text/html'
                     ? path.join(__dirname, 'views', req.url)
                     : path.join(__dirname, req.url);
+    
+    // faz com que .html extensão não requerida no navegador
+    if(!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+    const fileExists = fs.existsSync(filePath);
+
+    if(fileExists){
+        // serve o arquivo
+        serveFile(filePath, contentType, res);
+    }else{
+        //404
+        // 301 redirect
+        switch(path.parse(filePath).base){
+            case 'old-page.html':
+                res.writeHead(301,{
+                    'Location': '/new-page.html'
+                });
+                res.end();
+                break;
+            case 'www-page.html':
+                res.writeHead(301,{
+                    'Location': '/'
+                });
+                res.end();
+                break;
+            default:
+                //serve 404 response
+                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+        }
+    }
 });
 
 server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
 
 
 
-// myEmitter.on('log',(msg)=>logEvents(msg));
 
-//     myEmitter.emit('log','Log event emitted!');
+
